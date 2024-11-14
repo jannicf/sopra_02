c80ffb8131884beb2e520821c8992db7556f816b
jannicfriese <jannic.frs@gmail.com>
2024-11-14 16:18:14 +0100

    BusinessObject Klassen Kleidungstyp, Outfit und Style erstellt

---
+++ src/server/bo/Kleidungstyp.py
@@ -0,0 +1,5 @@
+from src.server.bo.BusinessObject import BusinessObject as bo
+
+
+class Kleidungstyp(bo.BusinessObject):
+    pass
---
+++ src/server/bo/Outfit.py
@@ -0,0 +1,5 @@
+from src.server.bo.BusinessObject import BusinessObject as bo
+
+
+class Outfit(bo.BusinessObject):
+    pass
