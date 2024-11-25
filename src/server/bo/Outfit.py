78e9d160493034bdfda9a8b52a0393c898710021
jannicfriese <jannic.frs@gmail.com>
2024-11-25 12:53:06 +0100

    requirements.txt erstellt

--- src/server/bo/Outfit.py
+++ src/server/bo/Outfit.py
@@ -1,5 +1,32 @@
 from src.server.bo import BusinessObject as bo
-
+from src.server.bo import Kleidungsstueck
 
 class Outfit(bo.BusinessObject):
-    pass
+
+    def __init__(self):
+        super().__init__()
+        self.__bausteine = [] # die Bestandteile eines Outfits bestehend aus Kleidungsstücken
+
+    def add_baustein(self, kleidungsstueck: Kleidungsstueck):
+        """Fügt ein Kleidungsstück zum Outfit hinzu"""
+        self.__bausteine.append(kleidungsstueck)
+
+    def remove_feature(self, kleidungsstueck: Kleidungsstueck):
+        """Entfernt ein Kleidungsstück aus dem Outfit"""
+        if kleidungsstueck in self.__bausteine:
+            self.__bausteine.remove(kleidungsstueck)
+
+    def get_features(self):
+        """Gibt alle Kleidungsstücke des Outfits zurück"""
+        return self.__bausteine
+
+    def __str__(self) -> str:
+        """Umwandlung des Objekts in eine lesbare String-Ausgabe"""
+        return "Outfit: {}".format(self.get_id())
+
+    @staticmethod
+    def from_dict(dictionary=dict()):
+        """Umwandeln eines Python dict() in ein Outfit()."""
+        obj = Outfit()
+        obj.set_id(dictionary["id"])  # eigentlich Teil von BusinessObject !
+        return obj
