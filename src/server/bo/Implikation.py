f1abc831e86ff063484c79948bece7cd75879428
jannicfriese <jannic.frs@gmail.com>
2024-11-25 16:49:25 +0100

    BO-Klasse Implikation.py vollständig implementiert

--- src/server/bo/Implikation.py
+++ src/server/bo/Implikation.py
@@ -1,4 +1,46 @@
 from src.server.bo import BinaryConstraint
+from src.server.bo.Kleidungsstueck import Kleidungsstueck
+# Kleidungsstueck importieren um die Methode get_typ() zu verwenden
 
 class Implikation(BinaryConstraint):
-    pass
+
+    def __init__(self):
+        super().__init__()
+
+    def check_constraint(self, kleidungsstuecke):
+
+        """Überprüft, ob der Implikation-Constraint erfüllt ist.
+        Returns True wenn der Constraint erfüllt ist (keine Verletzung),
+        False wenn er verletzt ist."""
+
+        if not self.get_bezugsobjekt1() or not self.get_bezugsobjekt2():
+            return True
+
+        typ1_vorhanden = False
+        typ2_vorhanden = False
+
+        for kleidungsstueck in kleidungsstuecke:
+
+            """prüfen, ob die übergebenen kleidungsstücke 
+            vom selben Typ sind wie das Bezugsobjekt"""
+
+            if kleidungsstueck.get_typ() == self.get_bezugsobjekt1():
+                typ1_vorhanden = True
+            if kleidungsstueck.get_typ() == self.get_bezugsobjekt2():
+                typ2_vorhanden = True
+
+        # Wenn Typ1 vorhanden ist, muss auch Typ2 vorhanden sein
+        # Wenn Typ1 nicht vorhanden ist, ist die Bedingung immer erfüllt
+        return not typ1_vorhanden or typ2_vorhanden
+
+    def __str__(self) -> str:
+        """Umwandlung des Objekts in eine lesbare String-Ausgabe"""
+        return "Implikation: {}".format(self.get_id())
+
+    @staticmethod
+    def from_dict(dictionary=dict()):
+        """Umwandeln eines Python dict() in eine Implikation()."""
+        obj = Implikation()
+        obj.set_id(dictionary["id"])  # eigentlich Teil von BusinessObject !
+        return obj
+
