e6215535f8b06dad960194bd86d7c287c8c62c2d
jannicfriese <jannic.frs@gmail.com>
2024-11-25 23:42:08 +0100

    Context Manager der Mapper-Basisklasse implementiert

--- src/server/db/Mapper.py
+++ src/server/db/Mapper.py
@@ -1,8 +1,35 @@
 from abc import ABC, abstractmethod
+import mysql.connector as connector
+from contextlib import AbstractContextManager
+import os
 
-class Mapper(ABC):
-    def __init__(self, db_connection):
-        self.db_connection = db_connection
+class Mapper(AbstractContextManager, ABC):
+    def __init__(self):
+        self.__cnx = None # cnx steht für unsere aufgebaute Verbindung zur Datenbank
+
+    def __enter__(self):
+
+        if os.getenv('GAE_ENV', '').startswith('standard'):
+            """Landen wir in diesem Zweig, so haben wir festgestellt, dass der Code in der Cloud abläuft.
+            Die App befindet sich somit im **Production Mode** und zwar im *Standard Environment*.
+            Hierbei handelt es sich also um die Verbindung zwischen Google App Engine und Cloud SQL."""
+
+            self._cnx = connector.connect(user='root', password='peterthies',
+                                          unix_socket='#hier kommt google cloud verbindung hin#',
+                                          database='sopra_db')
+        else:
+            """Wenn wir hier ankommen, dann handelt sich offenbar um die Ausführung des Codes in einer lokalen Umgebung,
+            also auf einem Local Development Server. Hierbei stellen wir eine einfache Verbindung zu einer lokal
+            installierten mySQL-Datenbank her."""
+
+            self._cnx = connector.connect(host='localhost', user='root', password='peterthies',
+                                          database='sopra_db')
+
+        return self
+
+    def __exit__(self, exc_type, exc_val, exc_tb):
+        self._cnx.close()
+
 
     @abstractmethod
     def insert(self, obj):
