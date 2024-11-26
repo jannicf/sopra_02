417eb5ffb5e477932b29b9be7644c76a0bf489d7
yasinyasar <yasinyasar2002@gmail.com>
2024-11-26 13:40:42 +0100

    Klasse PersonMapper implementiert

--- src/server/db/PersonMapper.py
+++ src/server/db/PersonMapper.py
@@ -1,17 +1,256 @@
 from src.server.db.Mapper import Mapper
+from src.server.bo.Person import Person
 
 class PersonMapper(Mapper):
     def insert(self, person):
-        pass
+        """Einfügen eines User-Objekts in die Datenbank.
+
+                Dabei wird auch der Primärschlüssel des übergebenen Objekts geprüft und ggf.
+                berichtigt.
+
+                :param user das zu speichernde Objekt
+                :return das bereits übergebene Objekt, jedoch mit ggf. korrigierter ID.
+                """
+        cursor = self._cnx.cursor()
+        cursor.execute("SELECT MAX(id) AS maxid FROM person ")
+        tuples = cursor.fetchall()
+
+        for (maxid) in tuples:
+            if maxid[0] is not None:
+                """Wenn wir eine maximale ID festellen konnten, zählen wir diese
+                um 1 hoch und weisen diesen Wert als ID dem User-Objekt zu."""
+                person.set_id(maxid[0] + 1)
+            else:
+                """Wenn wir KEINE maximale ID feststellen konnten, dann gehen wir
+                davon aus, dass die Tabelle leer ist und wir mit der ID 1 beginnen können."""
+                person.set_id(1)
+
+        command = "INSERT INTO person (id, vorname, nachname, nickname, google_id) VALUES (%s,%s,%s,%s,%s)"
+        data = (person.get_id(), person.get_vorname(), person.get_nachname(), person.get_nickname(),
+                person.get_google_id())
+        cursor.execute(command, data)
+
+        self._cnx.commit()
+        cursor.close()
+
+        return person
 
     def update(self, person):
-        pass
+        """Wiederholtes Schreiben eines Objekts in die Datenbank.
+
+                :param user das Objekt, das in die DB geschrieben werden soll
+                """
+        cursor = self._cnx.cursor()
+
+        command = "UPDATE person " + "SET vorname=%s, nachname=%s, nickname=%s, google_id=%s WHERE id=%s"
+        data = (person.get_vorname(), person.get_nachname(), person.get_nickname(),
+                person.get_google_id(), person.get_id())
+        cursor.execute(command, data)
+
+        self._cnx.commit()
+        cursor.close()
 
     def delete(self, person):
-        pass
+        """Löschen der Daten eines User-Objekts aus der Datenbank.
+
+                :param user das aus der DB zu löschende "Objekt"
+                """
+        cursor = self._cnx.cursor()
+
+        command = "DELETE FROM person WHERE id={}".format(person.get_id())
+        cursor.execute(command)
+
+        self._cnx.commit()
+        cursor.close()
 
     def find_by_id(self, person_id):
-        pass
+        """Suchen eines Benutzers mit vorgegebener User ID. Da diese eindeutig ist,
+                wird genau ein Objekt zurückgegeben.
+
+                :param key Primärschlüsselattribut (->DB)
+                :return User-Objekt, das dem übergebenen Schlüssel entspricht, None bei
+                    nicht vorhandenem DB-Tupel.
+                """
+
+        result = None
+
+        cursor = self._cnx.cursor()
+        command = "SELECT id, vorname, nachname, nickname, google_id FROM person WHERE id={}".format(person_id)
+        cursor.execute(command)
+        tuples = cursor.fetchall()
+
+        try:
+            (id, vorname, nachname, nickname, google_id) = tuples[0]
+            person = Person()
+            person.set_id(id)
+            person.set_vorname(vorname)
+            person.set_nachname(nachname)
+            person.set_nickname(nickname)
+            person.set_google_id(google_id)
+            result = person
+        except IndexError:
+            """Der IndexError wird oben beim Zugriff auf tuples[0] auftreten, wenn der vorherige SELECT-Aufruf
+            keine Tupel liefert, sondern tuples = cursor.fetchall() eine leere Sequenz zurück gibt."""
+            result = None
+
+        self._cnx.commit()
+        cursor.close()
+
+        return result
+
+    def find_by_vorname(self, vorname):
+        """Auslesen aller Benutzer anhand der zugeordneten E-Mail-Adresse.
+
+        :param mail_address E-Mail-Adresse der zugehörigen Benutzer.
+        :return Eine Sammlung mit User-Objekten, die sämtliche Benutzer
+            mit der gewünschten E-Mail-Adresse enthält.
+        """
+        result = None
+
+        cursor = self._cnx.cursor()
+        command = "SELECT id, vorname, nachname, nickname, google_id FROM person WHERE vorname={}".format(vorname)
+        cursor.execute(command)
+        tuples = cursor.fetchall()
+
+        try:
+            (id, vorname, nachname, nickname, google_id) = tuples[0]
+            person = Person()
+            person.set_id(id)
+            person.set_vorname(vorname)
+            person.set_nachname(nachname)
+            person.set_nickname(nickname)
+            person.set_google_id(google_id)
+            result = person
+        except IndexError:
+            """Der IndexError wird oben beim Zugriff auf tuples[0] auftreten, wenn der vorherige SELECT-Aufruf
+            keine Tupel liefert, sondern tuples = cursor.fetchall() eine leere Sequenz zurück gibt."""
+            result = None
+
+        self._cnx.commit()
+        cursor.close()
+
+        return result
+
+    def find_by_nachname(self, nachname):
+        """Auslesen aller Benutzer anhand der zugeordneten E-Mail-Adresse.
+
+        :param mail_address E-Mail-Adresse der zugehörigen Benutzer.
+        :return Eine Sammlung mit User-Objekten, die sämtliche Benutzer
+            mit der gewünschten E-Mail-Adresse enthält.
+        """
+        result = None
+
+        cursor = self._cnx.cursor()
+        command = "SELECT id, vorname, nachname, nickname, google_id FROM person WHERE nachname={}".format(nachname)
+        cursor.execute(command)
+        tuples = cursor.fetchall()
+
+        try:
+            (id, vorname, nachname, nickname, google_id) = tuples[0]
+            person = Person()
+            person.set_id(id)
+            person.set_vorname(vorname)
+            person.set_nachname(nachname)
+            person.set_nickname(nickname)
+            person.set_google_id(google_id)
+            result = person
+        except IndexError:
+            """Der IndexError wird oben beim Zugriff auf tuples[0] auftreten, wenn der vorherige SELECT-Aufruf
+            keine Tupel liefert, sondern tuples = cursor.fetchall() eine leere Sequenz zurück gibt."""
+            result = None
+
+        self._cnx.commit()
+        cursor.close()
+
+        return result
+
+    def find_by_nickname(self, nickname):
+        """Auslesen aller Benutzer anhand der zugeordneten E-Mail-Adresse.
+
+        :param mail_address E-Mail-Adresse der zugehörigen Benutzer.
+        :return Eine Sammlung mit User-Objekten, die sämtliche Benutzer
+            mit der gewünschten E-Mail-Adresse enthält.
+        """
+        result = None
+
+        cursor = self._cnx.cursor()
+        command = "SELECT id, vorname, nachname, nickname, google_id FROM person WHERE nickname={}".format(nickname)
+        cursor.execute(command)
+        tuples = cursor.fetchall()
+
+        try:
+            (id, vorname, nachname, nickname, google_id) = tuples[0]
+            person = Person()
+            person.set_id(id)
+            person.set_vorname(vorname)
+            person.set_nachname(nachname)
+            person.set_nickname(nickname)
+            person.set_google_id(google_id)
+            result = person
+        except IndexError:
+            """Der IndexError wird oben beim Zugriff auf tuples[0] auftreten, wenn der vorherige SELECT-Aufruf
+            keine Tupel liefert, sondern tuples = cursor.fetchall() eine leere Sequenz zurück gibt."""
+            result = None
+
+        self._cnx.commit()
+        cursor.close()
+
+        return result
+
+    def find_by_google_id(self, google_id):
+        """Auslesen aller Benutzer anhand der zugeordneten E-Mail-Adresse.
+
+        :param mail_address E-Mail-Adresse der zugehörigen Benutzer.
+        :return Eine Sammlung mit User-Objekten, die sämtliche Benutzer
+            mit der gewünschten E-Mail-Adresse enthält.
+        """
+        result = None
+
+        cursor = self._cnx.cursor()
+        command = "SELECT id, vorname, nachname, nickname, google_id FROM person WHERE google_id={}".format(google_id)
+        cursor.execute(command)
+        tuples = cursor.fetchall()
+
+        try:
+            (id, vorname, nachname, nickname, google_id) = tuples[0]
+            person = Person()
+            person.set_id(id)
+            person.set_vorname(vorname)
+            person.set_nachname(nachname)
+            person.set_nickname(nickname)
+            person.set_google_id(google_id)
+            result = person
+        except IndexError:
+            """Der IndexError wird oben beim Zugriff auf tuples[0] auftreten, wenn der vorherige SELECT-Aufruf
+            keine Tupel liefert, sondern tuples = cursor.fetchall() eine leere Sequenz zurück gibt."""
+            result = None
+
+        self._cnx.commit()
+        cursor.close()
+
+        return result
 
     def find_all(self):
-        pass
+        """Auslesen aller Benutzer unseres Systems.
+
+                :return Eine Sammlung mit User-Objekten, die sämtliche Benutzer
+                        des Systems repräsentieren.
+                """
+        result = []
+        cursor = self._cnx.cursor()
+        cursor.execute("SELECT * from person")
+        tuples = cursor.fetchall()
+
+        for (id, vorname, nachname, nickname, google_id) in tuples:
+            person = Person()
+            person.set_id(id)
+            person.set_vorname(vorname)
+            person.set_nachname(nachname)
+            person.set_nickname(nickname)
+            person.set_google_id(google_id)
+            result.append(person)
+
+        self._cnx.commit()
+        cursor.close()
+
+        return result
