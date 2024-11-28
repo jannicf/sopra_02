5540a1540c6af2ef768a2870830d6b2b1e7653ba
jannicfriese <jannic.frs@gmail.com>
2024-11-28 14:36:06 +0100

    kleinere Kommentarveränderungen

--- src/server/db/MutexMapper.py
+++ src/server/db/MutexMapper.py
@@ -19,7 +19,7 @@ class MutexMapper(Mapper):
         for (maxid) in tuples:
             if maxid[0] is not None:
                 """Wenn wir eine maximale ID festellen konnten, zählen wir diese
-                um 1 hoch und weisen diesen Wert als ID dem User-Objekt zu."""
+                um 1 hoch und weisen diesen Wert als ID dem Mutex-Objekt zu."""
                 mutex.set_id(maxid[0] + 1)
             else:
                 """Wenn wir KEINE maximale ID feststellen konnten, dann gehen wir
