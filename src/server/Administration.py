415a46ee493e5f27fddc0f4adf629a59ba5dec2b
yasinyasar <yasinyasar2002@gmail.com>
2024-11-29 10:37:45 +0100

    Klasse KleidungsstueckMapper angepasst und Administration

--- src/server/Administration.py
+++ src/server/Administration.py
@@ -1,6 +1,46 @@
+from src.server.bo.Person import Person
+from src.server.db.PersonMapper import PersonMapper
 
 class WardrobeAdministration(object):
     """Diese Klasse aggregiert sämtliche Business Logik der Anwendung"""
 
     def __init__(self):
         pass
+
+        """
+        Person-spezifische Methoden
+        """
+
+    def create_person(self, vorname, nachname, nickname, google_id):
+        """Eine neue Person anlegen."""
+        person = Person()
+        person.set_vorname(vorname)
+        person.set_nachname(nachname)
+        person.set_nickname(nickname)
+        person.set_google_id(google_id)
+        person.set_id(1)
+
+        with PersonMapper() as mapper:
+            return mapper.insert(person)
+
+    def get_person_by_id(self, number):
+        """Die Person mit der gegebenen ID auslesen."""
+        with PersonMapper() as mapper:
+            return mapper.find_by_id(number)
+
+    def get_person_by_google_id(self, id):
+        """Die Person mit der gegebenen Google ID auslesen."""
+        with PersonMapper() as mapper:
+            return mapper.find_by_google_id(id)
+
+    def save_person(self, person):
+        """Die gegebene Person speichern."""
+        with PersonMapper() as mapper:
+            mapper.update(person)
+
+    def delete_person(self, person):
+        """Die gegebene Person aus unserem System löschen."""
+        with PersonMapper() as mapper:
+            if person.get_kleiderschrank() is not None:
+                self.delete_kleiderschrank(person.get_kleiderschrank())
+            mapper.delete(person)
--- src/server/db/KleidungsstueckMapper.py
+++ src/server/db/KleidungsstueckMapper.py
@@ -1,18 +1,173 @@
 from src.server.db.Mapper import Mapper
-
+from src.server.bo.Kleidungsstueck import Kleidungsstueck
 
 class KleidungsstueckMapper(Mapper):
     def insert(self, kleidungsstueck):
-        pass
+        """Einfügen eines Kleidungsstück-Objekts in die Datenbank.
+        Dabei wird auch der Primärschlüssel des übergebenen Objekts geprüft und ggf.
+        berichtigt.
+        :param kleidungsstueck das zu speichernde Objekt
+        :return das bereits übergebene Objekt, jedoch mit ggf. korrigierter ID."""
+        cursor = self._cnx.cursor()
+        cursor.execute("SELECT MAX(id) AS maxid FROM kleidungsstueck ")
+        tuples = cursor.fetchall()
+
+        for (maxid) in tuples:
+            if maxid[0] is not None:
+                """Wenn wir eine maximale ID festellen konnten, zählen wir diese
+                um 1 hoch und weisen diesen Wert als ID dem Kleidungsstück-Objekt zu."""
+                kleidungsstueck.set_id(maxid[0] + 1)
+            else:
+                """Wenn wir KEINE maximale ID feststellen konnten, dann gehen wir
+                davon aus, dass die Tabelle leer ist und wir mit der ID 1 beginnen können."""
+                kleidungsstueck.set_id(1)
+
+        command = "INSERT INTO kleidungsstueck (id, name, typ) VALUES (%s,%s,%s)"
+        data = (kleidungsstueck.get_id(), kleidungsstueck.get_name(), kleidungsstueck.get_typ())
+        cursor.execute(command, data)
+
+        self._cnx.commit()
+        cursor.close()
+
+        return kleidungsstueck
 
     def update(self, kleidungsstueck):
-        pass
+        """Wiederholtes Schreiben eines Objekts in die Datenbank.
+
+        :param kleidungsstueck das Objekt, das in die DB geschrieben werden soll"""
+        cursor = self._cnx.cursor()
+
+        command = "UPDATE kleidungsstueck " + "SET name=%s, typ=%s WHERE id=%s"
+        data = (kleidungsstueck.get_name(), kleidungsstueck.get_typ())
+        cursor.execute(command, data)
+
+        self._cnx.commit()
+        cursor.close()
 
     def delete(self, kleidungsstueck):
-        pass
+        """Löschen der Daten eines Kleidungsstück-Objekts aus der Datenbank.
+
+        :param kleidungsstueck das aus der DB zu löschende "Objekt"
+        """
+        cursor = self._cnx.cursor()
+
+        command = "DELETE FROM kleidungsstueck WHERE id={}".format(kleidungsstueck.get_id())
+        cursor.execute(command)
+
+        self._cnx.commit()
+        cursor.close()
 
     def find_by_id(self, kleidungsstueck_id):
-        pass
+        """Suchen eines Kleidungsstücks mit vorgegebener Kleidungsstück ID. Da diese eindeutig ist,
+                        wird genau ein Objekt zurückgegeben.
+        :param kleidungsstueck_id Primärschlüsselattribut (->DB)
+        :return Kleidungsstück-Objekt, das dem übergebenen Schlüssel entspricht, None bei nicht vorhandenem DB-Tupel."""
+
+        result = None
+
+        cursor = self._cnx.cursor()
+        command = "SELECT id, name, typ FROM kleidungsstueck WHERE id={}".format(kleidungsstueck_id)
+        cursor.execute(command)
+        tuples = cursor.fetchall()
+
+        try:
+            (id, name, typ) = tuples[0]
+            kleidungsstueck = Kleidungsstueck()
+            kleidungsstueck.set_id(id)
+            kleidungsstueck.set_name(name)
+            kleidungsstueck.set_typ(typ)
+            result = kleidungsstueck
+        except IndexError:
+            """Der IndexError wird oben beim Zugriff auf tuples[0] auftreten, wenn der vorherige SELECT-Aufruf
+            keine Tupel liefert, sondern tuples = cursor.fetchall() eine leere Sequenz zurück gibt."""
+            result = None
+
+        self._cnx.commit()
+        cursor.close()
+
+        return result
+
+    def find_by_name(self, name):
+        """Auslesen aller Kleidungsstücke anhand der zugeordneten Name.
+
+        :param name Name des zugehörigen Kleidungsstücks.
+        :return Eine Sammlung mit Kleidungsstück-Objekten, die sämtliche Kleidungsstücke
+            mit dem gewünschten Namen enthält.
+        """
+        result = None
+
+        cursor = self._cnx.cursor()
+        command = "SELECT id, name, typ FROM kleidungsstueck WHERE name={}".format(name)
+        cursor.execute(command)
+        tuples = cursor.fetchall()
+
+        try:
+            (id, name, typ) = tuples[0]
+            kleidungsstueck = Kleidungsstueck()
+            kleidungsstueck.set_id(id)
+            kleidungsstueck.set_name(name)
+            kleidungsstueck.set_typ(typ)
+            result = kleidungsstueck
+        except IndexError:
+            """Der IndexError wird oben beim Zugriff auf tuples[0] auftreten, wenn der vorherige SELECT-Aufruf
+            keine Tupel liefert, sondern tuples = cursor.fetchall() eine leere Sequenz zurück gibt."""
+            result = None
+
+        self._cnx.commit()
+        cursor.close()
+
+        return result
+
+    def find_by_typ(self, typ):
+        """Auslesen aller Kleidungsstücke anhand des zugeordneten Typs.
+
+        :param typ Typ des zugehörigen Kleidungsstücks.
+        :return Eine Sammlung mit Kleidungsstück-Objekten, die sämtliche Kleidungsstücke
+            mit dem gewünschtem Typ enthält.
+        """
+        result = None
+
+        cursor = self._cnx.cursor()
+        command = "SELECT id, name, typ FROM kleidungsstueck WHERE typ={}".format(typ)
+        cursor.execute(command)
+        tuples = cursor.fetchall()
+
+        try:
+            (id, name, typ) = tuples[0]
+            kleidungsstueck = Kleidungsstueck()
+            kleidungsstueck.set_id(id)
+            kleidungsstueck.set_name(name)
+            kleidungsstueck.set_typ(typ)
+            result = kleidungsstueck
+        except IndexError:
+            """Der IndexError wird oben beim Zugriff auf tuples[0] auftreten, wenn der vorherige SELECT-Aufruf
+            keine Tupel liefert, sondern tuples = cursor.fetchall() eine leere Sequenz zurück gibt."""
+            result = None
+
+        self._cnx.commit()
+        cursor.close()
+
+        return result
 
     def find_all(self):
-        pass
+        """Auslesen aller Kleidungsstücke unseres Systems.
+
+        :return Eine Sammlung mit Kleidungsstück-Objekten, die sämtliche Kleidungsstücke
+        des Systems repräsentieren.
+        """
+        result = []
+        cursor = self._cnx.cursor()
+        cursor.execute("SELECT * from kleidungsstueck")
+        tuples = cursor.fetchall()
+
+        for (id, name, typ) in tuples:
+            kleidungsstueck = Kleidungsstueck()
+            kleidungsstueck.set_id(id)
+            kleidungsstueck.set_name(name)
+            kleidungsstueck.set_typ(typ)
+            result.append(kleidungsstueck)
+
+        self._cnx.commit()
+        cursor.close()
+
+        return result
