6340d58422478ffb9c7e9b66416a9198b059fb8b
josefstuby <stbjsf@gmail.com>
2024-12-12 11:13:40 +0100

    Ordnerstruktur für Frontend und Firebase

---
+++ frontend/src/firebase/FireBaseLogin.js
@@ -0,0 +1,16 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyAnM_OGFZiDFgWBLlrHGDI8pEL1jbKPaxA",
+  authDomain: "sopra-kleiderschrank.firebaseapp.com",
+  projectId: "sopra-kleiderschrank",
+  storageBucket: "sopra-kleiderschrank.firebasestorage.app",
+  messagingSenderId: "149184484663",
+  appId: "1:149184484663:web:aa67fce4f30226301ff6b2"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
