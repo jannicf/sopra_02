59e4fb76cfcf767ab32af7c0a3983db0ec9f7f7b
josefstuby <stbjsf@gmail.com>
2025-01-07 22:05:44 +0100

    Anpassung von footer.js

--- frontend/src/layout/footer.js
+++ frontend/src/layout/footer.js
@@ -1,59 +1,90 @@
-import React, { Component } from 'react';
-import { Paper, Typography, Tabs, Tab } from '@mui/material';
+import React from 'react';
+import { Paper, Typography, Grid, Box, Link, Chip, Tooltip } from '@mui/material';
 import { Link as RouterLink } from 'react-router-dom';
+import SchoolIcon from '@mui/icons-material/School';
 
-class Footer extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            tabindex: 0
-        };
-    }
+const Footer = ({ user }) => {
+  if (!user) return null;
 
-    // Event-Handler für Tab-Änderungen
-    handleTabChange = (e, newIndex) => {
-        this.setState({
-            tabindex: newIndex
-        });
-    };
+  const groupMembers = [
+    'Boris Burkert',
+    'Jannic Friese',
+    'Josef Stuby',
+    'Lucas Urban',
+    'Xiaoping Wu',
+    'Yasin Yasar'
+  ];
 
-    render() {
-        const { user } = this.props;
-
-        return (
-            <Paper
-                variant='outlined'
-                sx={{
-                    position: 'fixed',
-                    bottom: 0,
-                    left: 0,
-                    right: 0,
-                    marginTop: 2,
-                    display: 'flex',
-                    justifyContent: 'space-between',
-                    alignItems: 'center',
-                    px: 2
-                }}
-            >
-                {/* Footer erst Anzeigen wenn User eingeloggt ist */}
-                {
-                user ?
-                <Typography variant='h6' align='center'>
-                    SoPra WS 24/25 - Gruppe 02
+  return (
+    <Box sx={{ pb: '48px' }}>
+      <Paper
+        variant="outlined"
+        sx={{
+          position: 'fixed',
+          bottom: 0,
+          left: 0,
+          right: 0,
+          bgcolor: 'background.paper',
+          py: 1
+        }}
+      >
+        <Grid container sx={{ px: 3 }} alignItems="center">
+          <Grid item xs={12} md={6}>
+            <Box sx={{ display: 'flex', alignItems: 'center', gap: 1 }}>
+              <Typography variant="subtitle1" color="primary">
+                SoPra
+              </Typography>
+              <Chip
+                label="WiSe 2024/25"
+                size="small"
+                color="primary"
+                variant="outlined"
+                sx={{ height: '20px' }}
+              />
+              <Tooltip title={groupMembers.join(', ')} placement="top">
+                <Typography variant="body2" color="text.secondary" sx={{ ml: 1 }}>
+                  Gruppe 02
                 </Typography>
-                : null
-                }
-            {
-              user ?
-                <Tabs indicatorColor='primary' textColor='primary' value={this.state.tabindex} onChange={this.handleTabChange} >
-                  <Tab label='Über uns' component={RouterLink} to='/about' />
-                </Tabs>
-                : null
-            }
-          </Paper>
-        )
-  }
-}
+              </Tooltip>
+            </Box>
+          </Grid>
+
+          <Grid item xs={12} md={6}>
+            <Box sx={{ display: 'flex', gap: 2, justifyContent: { md: 'flex-end' }, alignItems: 'center' }}>
+              <Link
+                component={RouterLink}
+                to="/about"
+                color="primary"
+                underline="hover"
+                sx={{ typography: 'body2' }}
+              >
+                Über uns
+              </Link>
+              <Link
+                href="https://www.hdm-stuttgart.de"
+                color="primary"
+                underline="hover"
+                target="_blank"
+                sx={{ display: 'flex', alignItems: 'center', gap: 0.5, typography: 'body2' }}
+              >
+                <SchoolIcon sx={{ fontSize: 16 }} />
+                HdM Stuttgart
+              </Link>
+              <Link
+                href="https://moodle.hdm-stuttgart.de/course/view.php?id=808"
+                color="primary"
+                underline="hover"
+                target="_blank"
+                sx={{ typography: 'body2' }}
+              >
+                Moodle
+              </Link>
+            </Box>
+          </Grid>
+        </Grid>
+      </Paper>
+    </Box>
+  );
+};
 
 export default Footer;
-
